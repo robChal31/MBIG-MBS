7736eb3a8d4b2046b5bff5de8d108d72e8feac18
robchal <robanichalif.aa@gmail.com>
2024-06-13 09:54:54 +0700

    adding draft pk v2

--- save-draft-pk.php
+++ save-draft-pk.php
@@ -197,7 +197,7 @@
         // ];
 
         $cc[] = [
-            'email' => "bany@mentarigroups.com",
+            'email' => "secretary@mentaribooks.com",
             'name' => "Putri"
         ];
 
