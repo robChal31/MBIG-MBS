5cdd5b40492a85977792137f854dbe1ad1e619d0
robchal <robanichalif.aa@gmail.com>
2024-06-21 16:20:40 +0700

    fixing approve history and save old pkv5

--- input-book.php
+++ input-book.php
@@ -16,7 +16,7 @@ $draft_exec = mysqli_query($conn, $draft_book_q);
 if (mysqli_num_rows($draft_exec) > 0) {
   $books = mysqli_fetch_all($draft_exec, MYSQLI_ASSOC);    
 }
-$book = $books[0] ? $books[0] : [];
+$book = $books[0] ?? [];
 
 ?>
     <div class="p-2">
@@ -25,10 +25,10 @@ $book = $books[0] ? $books[0] : [];
             <div class="row">
                 <div class="col-12 mb-3">
                     <label class="form-label" style="font-size: .85rem;">Title</label>
-                    <input type="text" name="name" class="form-control form-control-sm" value="<?= $book['name'] ?>" placeholder="book name..." required>
+                    <input type="text" name="name" class="form-control form-control-sm" value="<?= $book['name'] ?? '' ?>" placeholder="book name..." required>
                 </div>
                 
-                <input type="hidden" name="id_book" value="<?= $id_book ?>">
+                <input type="hidden" name="id_book" value="<?= $id_book == 0 ? '' : $id_book ?>">
             </div>
 
             <div class="d-flex justify-content-end">
