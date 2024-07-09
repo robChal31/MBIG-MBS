4c5015d84004997639fe87a29870d307e891874c
robchal <robanichalif.aa@gmail.com>
2024-07-09 10:00:22 +0700

    fixing accumlate on benefit draftv5

--- get_benefits.php
+++ get_benefits.php
@@ -26,40 +26,44 @@
     $filter_program_q = $program_code ? "AND avail like '%$program_code%' " : false;
     $options = "";
 
-    if($filter_program_q) {
-      if(count($selected_template) > 0) {
-        $sql = "SELECT * FROM `draft_template_benefit` WHERE is_active = 1 $filter_program_q AND id_template_benefit NOT IN (" . implode(',', $selected_template) . ") order by benefit, subbenefit, benefit_name asc";
-      }else {
-        $sql = "SELECT * FROM `draft_template_benefit` WHERE is_active = 1 $filter_program_q order by benefit, subbenefit, benefit_name asc";
-      }
-      
-      // $sql = 'SELECT * FROM `draft_template_benefit` order by benefit,subbenefit,benefit_name asc';
-      $result = $conn->query($sql);
-      
-
-      if ($result->num_rows > 0) {
-        $options .= "<option value=''>Select Benefit</option>";
-        $number = 1;
-        while ($row = $result->fetch_assoc()) {
-          $option = $row['benefit_name'];
-          if($selected) {
-            $is_selected = $selected == $row['id_template_benefit'] ? 'selected' : '';
-          }
-          $options .= "<option value='".$row['id_template_benefit']."' ". $is_selected .">".$row['benefit']." - ".$row['subbenefit']." - ".$option."</option>";
-          $number++;
+    try {
+      if($filter_program_q) {
+        if(count($selected_template) > 0) {
+          $sql = "SELECT * FROM `draft_template_benefit` WHERE is_active = 1 $filter_program_q AND id_template_benefit NOT IN (" . implode(',', $selected_template) . ") order by benefit, subbenefit, benefit_name asc";
+        }else {
+          $sql = "SELECT * FROM `draft_template_benefit` WHERE is_active = 1 $filter_program_q order by benefit, subbenefit, benefit_name asc";
         }
-        $conn->close();
-    
-        echo $options;
+        
+        // $sql = 'SELECT * FROM `draft_template_benefit` order by benefit,subbenefit,benefit_name asc';
+        $result = $conn->query($sql);
+        
+  
+        if ($result->num_rows > 0) {
+          $options .= "<option value=''>Select Benefit</option>";
+          $number = 1;
+          while ($row = $result->fetch_assoc()) {
+            $option = $row['benefit_name'];
+            if($selected) {
+              $is_selected = $selected == $row['id_template_benefit'] ? 'selected' : '';
+            }
+            $options .= "<option value='".$row['id_template_benefit']."' ". $is_selected .">".$row['benefit']." - ".$row['subbenefit']." - ".$option."</option>";
+            $number++;
+          }
+          $conn->close();
+      
+          echo $options;
+        }else {
+          $conn->close();
+          echo "<option value=''>No Benefit Found</option>";
+        }
+       
       }else {
         $conn->close();
-        echo "<option value=''>No Benefit Found</option>";
+      
+        echo "<option value=''>Program Not Found</option>";
       }
-     
-    }else {
-      $conn->close();
-    
-      echo "<option value=''>Program Not Found</option>";
+    } catch (\Throwable $th) {
+      echo $th;
     }
 
       
